3c649e3ea133e45d4dbef3bb78228b2766f975ad
fadil <elking.fadel2@gmail.com>
2023-11-19 13:19:52 +0200

    edited

--- powerplant.cpp
+++ powerplant.cpp
@@ -7,16 +7,16 @@ float pw(float v, float p1, float p2)
     pw=v*(p2-p1);
     return pw;
 }
-float wout(float mf, float qin, float qout)
+float wout( float qin, float qout)
 {
     float wnet;
-    wnet=mf*(qin-qout);
+    wnet=qin-qout;
     return wnet;
 }
-float nth(float qin, float qout)
+float nth(float wnet, float qin)
 { 
     float nther;
-    nther=(1-(qout/qin))*100;
+    nther=wnet/qin*100;
     return nther;
 }
 float qin(float h3, float h2)
@@ -78,7 +78,7 @@ float ntha(float wnet_actual,float qin_actual)
 int main()
 {
 /*task1*/
-float p1_ideal=10 , p2_ideal=3000, p3_ideal=3000, p4_ideal=10,  v1_ideal=0.001010/*from table*/ , mf=20, h1_ideal=191.81,h2_ideal,h3_ideal=3100/*from chart*/
+float p1_ideal=10 , p2_ideal=3000, p3_ideal=3000, p4_ideal=10,  v1_ideal=0.001010/*from table*/ , mf=20, h1_ideal=191.81,h2_ideal,h3_ideal=3120/*from chart*/
 ,h4_ideal=2140/*from chart*/,qin_ideal, qout_ideal;
 float pw_ideal, wnet_ideal, nther_ideal;
 pw_ideal=pw(v1_ideal,p1_ideal,p2_ideal);
@@ -86,8 +86,8 @@ float x=0.81; /*from chart*/
 h2_ideal=h2(pw_ideal,h1_ideal);
 qin_ideal=qin(h3_ideal,h2_ideal);
 qout_ideal=qout(h4_ideal,h1_ideal);
-wnet_ideal=wout(mf,qin_ideal,qout_ideal);
-nther_ideal=nth(qin_ideal,qout_ideal);
+wnet_ideal=wout(qin_ideal,qout_ideal);
+nther_ideal=nth(wnet_ideal,qin_ideal);
 std::cout<<"  "<<std::endl;
 std::cout<<"qin ideal= "<<qin_ideal<<std::endl;
 std::cout<<"qout ideal= "<<qout_ideal<<std::endl;
@@ -100,21 +100,23 @@ float ptask2_1=3600/*1.2 boiler pressure*/, ptask2_2=4200/*1.4 boiler pressure*/
 float pwtask2_1=pw(v1_ideal,p1_ideal,ptask2_1);
 float h2task2_1=h2(pwtask2_1,h1_ideal);
 float h3task2_1=3100;/*from chart*/
-float h4task2_1=2180;/*from chat*/
+float h4task2_1=2120;/*from chat*/
 float qintask2_1=qin(h3task2_1,h2task2_1);
 float qouttask2_1=qout(h4task2_1,h1_ideal);
-float nther_task2_1=nth(qintask2_1,qouttask2_1);
+float wnet_task2_1=wout(qintask2_1,qouttask2_1);
+float nther_task2_1=nth(wnet_task2_1,qintask2_1);
 std::cout<<"  "<<std::endl;
 std::cout<<"qin for task 2 no.1= "<<qintask2_1<<std::endl;
 std::cout<<"qout for task 2 no.1= "<<qouttask2_1<<std::endl;
 std::cout<<"thermal efficiency for task 2 no.1= "<<nther_task2_1<<std::endl;
 float pwtask2_2=pw(v1_ideal,p1_ideal,ptask2_2);
 float h2task2_2=h2(pwtask2_2,h1_ideal);
-float h3task2_2=3080;/*from chart*/
-float h4task2_2=2160;/*from chart*/
+float h3task2_2=3090;/*from chart*/
+float h4task2_2=2080;/*from chart*/
 float qintask2_2=qin(h3task2_2,h2task2_2);
 float qouttask2_2=qout(h4task2_2,h1_ideal);
-float nther_task2_2=nth(qintask2_2,qouttask2_2);
+float wnet_task2_2=wout(qintask2_2,qouttask2_2);
+float nther_task2_2=nth(wnet_task2_2,qintask2_2);
 std::cout<<"  "<<std::endl;
 std::cout<<"qin for task 2 no.2= "<<qintask2_2<<std::endl;
 std::cout<<"qout for task 2 no.2= "<<qouttask2_2<<std::endl;
@@ -122,10 +124,11 @@ std::cout<<"thermal efficiency for task 2 no.2= "<<nther_task2_2<<std::endl;
 float pwtask2_3=pw(v1_ideal,p1_ideal,ptask2_1);
 float h2task2_3=h2(pwtask2_3,h1_ideal);
 float h3task2_3=3060;/*from chart*/
-float h4task2_3=2150;/*from chart*/
+float h4task2_3=2050;/*from chart*/
 float qintask2_3=qin(h3task2_3,h2task2_3);
 float qouttask2_3=qout(h4task2_3,h1_ideal);
-float nther_task2_3=nth(qintask2_3,qouttask2_3);std::cout<<"  "<<std::endl;
+float wnet_task2_3=wout(qintask2_3,qouttask2_3);
+float nther_task2_3=nth(wnet_task2_3,qintask2_3);
 std::cout<<"  "<<std::endl;
 std::cout<<"qin for task 2 no.3= "<<qintask2_3<<std::endl;
 std::cout<<"qout for task 2 no.3= "<<qouttask2_3<<std::endl;
@@ -133,10 +136,11 @@ std::cout<<"thermal efficiency for task 2 no.3= "<<nther_task2_3<<std::endl;
 float pwtask2_4=pw(v1_ideal,p1_ideal,ptask2_4);
 float h2task2_4=h2(pwtask2_4,h1_ideal);
 float h3task2_4=3050;/*from chart*/
-float h4task2_4=2120;/*from chart*/
+float h4task2_4=2030;/*from chart*/
 float qintask2_4=qin(h3task2_4,h2task2_4);
 float qouttask2_4=qout(h4task2_4,h1_ideal);
-float nther_task2_4=nth(qintask2_4,qouttask2_4);
+float wnet_task2_4=wout(qintask2_4,qouttask2_4);
+float nther_task2_4=nth(wnet_task2_4,qintask2_4);
 std::cout<<"  "<<std::endl;
 std::cout<<"qin for task 2 no.4= "<<qintask2_4<<std::endl;
 std::cout<<"qout for task 2 no.4= "<<qouttask2_4<<std::endl;
@@ -147,10 +151,11 @@ float v1task3_1=0.0010092;
 float pwtask3_1=pw(v1task3_1,ptask3_1,p3_ideal);
 float h1task3_1=182.586; /*interpolation*/
 float h2task3_1=h2(pwtask3_1,h1task3_1);
-float h4task3_1=2130;
+float h4task3_1=2140;
 float qintask3_1=qin(h3_ideal,h2task3_1);
 float qouttask3_1=qout(h4task3_1,h1task3_1);
-float nther_task3_1=nth(qintask3_1,qouttask3_1);
+float wnet_task3_1=wout(qintask3_1,qouttask3_1);
+float nther_task3_1=nth(wnet_task3_1,qintask3_1);
 std::cout<<"  "<<std::endl;
 std::cout<<"qin for task 3 no.1= "<<qintask3_1<<std::endl;
 std::cout<<"qout for task 3 no.1= "<<qouttask3_1<<std::endl;
@@ -159,10 +164,11 @@ float v1task3_2=0.0010084;
 float pwtask3_2=pw(v1task3_2,ptask3_2,p3_ideal);
 float h1task3_2=173.362; /*interpolation*/
 float h2task3_2=h2(pwtask3_2,h1task3_2);
-float h4task3_2=2100;
+float h4task3_2=2110;
 float qintask3_2=qin(h3_ideal,h2task3_2);
 float qouttask3_2=qout(h4task3_2,h1task3_2);
-float nther_task3_2=nth(qintask3_2,qouttask3_2);
+float wnet_task3_2=wout(qintask3_2,qouttask3_2);
+float nther_task3_2=nth(wnet_task3_2,qintask3_2);
 std::cout<<"  "<<std::endl;
 std::cout<<"qin for task 3 no.2= "<<qintask3_2<<std::endl;
 std::cout<<"qout for task 3 no.2= "<<qouttask3_2<<std::endl;
@@ -171,10 +177,11 @@ float v1task3_3=0.0010074;
 float pwtask3_3=pw(v1task3_3,ptask3_3,p3_ideal);
 float h1task3_3=182.586; /*interpolation*/
 float h2task3_3=h2(pwtask3_3,h1task3_3);
-float h4task3_3=2090;
+float h4task3_3=2100;
 float qintask3_3=qin(h3_ideal,h2task3_3);
 float qouttask3_3=qout(h4task3_3,h1task3_3);
-float nther_task3_3=nth(qintask3_3,qouttask3_3);
+float wnet_task3_3=wout(qintask3_3,qouttask3_3);
+float nther_task3_3=nth(wnet_task3_3,qintask3_3);
 std::cout<<"  "<<std::endl;
 std::cout<<"qin for task 3 no.3= "<<qintask3_3<<std::endl;
 std::cout<<"qout for task 3 no.3= "<<qouttask3_3<<std::endl;
@@ -183,10 +190,11 @@ float v1task3_4=0.0010062;
 float pwtask3_4=pw(v1task3_4,ptask3_4,p3_ideal);
 float h1task3_4=150.15; /*interpolation*/
 float h2task3_4=h2(pwtask3_4,h1task3_4);
-float h4task3_4=2070;/*from chart*/
+float h4task3_4=2090;/*from chart*/
 float qintask3_4=qin(h3_ideal,h2task3_4);
 float qouttask3_4=qout(h4task3_4,h1task3_4);
-float nther_task3_4=nth(qintask3_4,qouttask3_4);
+float wnet_task3_4=wout(qintask3_4,qouttask3_4);
+float nther_task3_4=nth(wnet_task3_4,qintask3_4);
 std::cout<<"  "<<std::endl;
 std::cout<<"qin for task 3 no.4= "<<qintask3_4<<std::endl;
 std::cout<<"qout for task 3 no.4= "<<qouttask3_4<<std::endl;
@@ -194,14 +202,14 @@ std::cout<<"thermal efficiency for task 3 no.4= "<<nther_task3_4<<std::endl;
 /*task4*/
 float pw_reheat=pw(v1_ideal,p1_ideal,p2_ideal);
 float h2_reheat=h2(pw_reheat,h1_ideal);
-float h4_reheat=2930;/*from chart*/
+float h4_reheat=2940;/*from chart*/
 float h5_reheat=3140;/*from chart*/
-float h6_reheat=2250;/*from chart*/
+float h6_reheat=2260;/*from chart*/
 float x_reheat=0.86;/*from chart*/
 float qin_reheat=qin_Reheat(h3_ideal,h2_reheat,h5_reheat,h4_reheat);
 float qout_reheat=qout_Reheat(h6_reheat,h1_ideal);
-float wnet_reheat=wout(mf,qin_reheat,qout_reheat);
-float nther_reheat=nth(qin_reheat,qout_reheat);
+float wnet_reheat=wout(qin_reheat,qout_reheat);
+float nther_reheat=nth(wnet_reheat,qin_reheat);
 std::cout<<"  "<<std::endl;
 std::cout<<"the reheat pumpwork  = "<<pw_reheat<<std::endl;
 std::cout<<"the reheat moisture content = "<<x_reheat<<std::endl;
@@ -226,9 +234,6 @@ std::cout<<"the actual qin  = "<<qin_actual<<std::endl;
 std::cout<<"the actual qout  = "<<qout_actual<<std::endl;
 std::cout<<"the actual work net = "<<wnet_actual<<std::endl;
 std::cout<<"the actual efficiency = "<<nth_actual<<std::endl;
-
-
-
 
 
 }
